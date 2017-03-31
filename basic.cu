c3e29a82ade2512c12404af8dd7a28121c4c90b5
cmccurl <cmccurl@g.clemson.edu>
2017-03-31 17:49:04 -0400

    Scott is a towel.

--- basic.cu
+++ basic.cu
@@ -9,6 +9,69 @@
 
 #define TILE_WIDTH 16
 
+//Code written for 1D arrays ahead of time
+//for CUDA implementation. 
+//
+//Assumptions: the 'arr' has been malloc'd
+//			   before the function call.
+//
+//Functionality: linearizes the traversal of
+//			     the 8x8 block before the Huffman
+//				 encoding scheme is used.
+void Traverse(char *block, char *arr)
+{
+	int count = 0;
+	int r = 0;
+	int c = 0;
+
+	while(count < 64)
+	{
+		//FIRST HALF OF TRAVERSE
+		if(c < 7)
+		{
+			//MOVE RIGHT
+			arr[count++] = block[r*8 + (c++)];
+			
+			//ALGORITHM ALWAYS ENDS HERE
+			if(count == 64)
+				break;
+		}	
+		else
+		{
+			//MOVE DOWN
+			arr[count++] = block[(r++)*8 + c];
+		}
+
+		//MOVE DOWN AND LEFT	
+		while((r>0) && (r<7) && (c>0) && (c<7))
+		{
+			arr[count++] = block[(r++)*8 + (c--)];
+		}
+
+
+
+		//SECOND HALF OF TRAVERSE
+		if(r < 7)
+		{
+			//MOVE DOWN
+			arr[count++] = block[(r--)*8 + c];
+		}
+		else
+		{
+			//MOVE RIGHT
+			arr[count++] = block[r*8 + (c++)];
+		}
+
+		//MOVE UP AND RIGHT	
+		while((r>0) && (r<7) && (c>0) && (c<7))
+		{
+			arr[count++] = block[(r--)*8 + (c++)];
+		}
+	}
+}
+
+
+
 int main (int argc, char **argv)
 {
 	int imageWidth, imageHeight, matSize;
