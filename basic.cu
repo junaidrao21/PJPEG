6234a855ffcaa9eb688090247af14cfd6d63757b
cmccurl <cmccurl@g.clemson.edu>
2017-04-02 16:14:37 -0400

    Successful Traverse Test

--- basic.cu
+++ basic.cu
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <math.h>
 
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include "image.h"
+//#include "image.h"
 
 #define TILE_WIDTH 16
 
@@ -43,9 +44,9 @@ void Traverse(char *block, char *arr)
 		}
 
 		//MOVE DOWN AND LEFT	
-		while((r>0) && (r<7) && (c>0) && (c<7))
+		while((r<7) && (c>0))
 		{
-			arr[count++] = block[(r++)*8 + (c--)];
+		    arr[count++] = block[(r++)*8 + (c--)];
 		}
 
 
@@ -54,7 +55,7 @@ void Traverse(char *block, char *arr)
 		if(r < 7)
 		{
 			//MOVE DOWN
-			arr[count++] = block[(r--)*8 + c];
+			arr[count++] = block[(r++)*8 + c];
 		}
 		else
 		{
@@ -63,7 +64,7 @@ void Traverse(char *block, char *arr)
 		}
 
 		//MOVE UP AND RIGHT	
-		while((r>0) && (r<7) && (c>0) && (c<7))
+		while((r>0) && (c<7))
 		{
 			arr[count++] = block[(r--)*8 + (c++)];
 		}
@@ -74,18 +75,18 @@ void Traverse(char *block, char *arr)
 
 int main (int argc, char **argv)
 {
-	int imageWidth, imageHeight, matSize;
-	int *hostInputImage, *hostOutputImage;
-	int *deviceInputImage, *deviceOutputImage;
+//	int imageWidth, imageHeight, matSize;
+//	int *hostInputImage, *hostOutputImage;
+//	int *deviceInputImage, *deviceOutputImage;
 
-	char test[64] = { 1,  2,  6,  7, 15, 16, 28, 29,
-					  3,  5,  8, 14, 17, 27, 30, 43,
-					  4,  9, 13, 18, 26, 31, 42, 44,
-					 10, 12, 19, 25, 32, 41, 45, 54,
-					 11, 20, 24, 33, 40, 46, 53, 55,
-					 21, 23, 34, 39, 47, 52, 56, 61,
-	 				 22, 35, 38, 48, 51, 57, 60, 62,
-					 36, 37, 49, 50, 58, 59, 63, 64};
+	char test[64] = {  1,  2,  6,  7, 15, 16, 28, 29,
+		          3,  5,  8, 14, 17, 27, 30, 43,
+	                  4,  9, 13, 18, 26, 31, 42, 44,
+			 10, 12, 19, 25, 32, 41, 45, 54,
+			 11, 20, 24, 33, 40, 46, 53, 55,
+			 21, 23, 34, 39, 47, 52, 56, 61,
+	 		 22, 35, 38, 48, 51, 57, 60, 62,
+			 36, 37, 49, 50, 58, 59, 63, 64};
 
 
 	char *A = (char *)calloc(64, sizeof(char));
@@ -97,23 +98,23 @@ int main (int argc, char **argv)
 
 	int i;
 	for(i=0; i<64; i++)
-		printf("B[%d] = (%c).\n", i, B[i]);
+		printf("B[%d] = (%d).\n", i, (int)B[i]);
 
     /*
 	
 	clock_t gpu_start, gpu_end;
 
-	/*if (argc != 5)
+	*if (argc != 5)
 	{
 		printf("Usage: ./lab2 <input-image> <output-image-name> <blur/gaussian/emboss/sharp> <1d/2d kernel>\n");
 		exit(1);
 	}*/
 
 	// Read in image and convert to readable format
-	read_image_template<int>(argv[1], &hostInputImage, &imageWidth, &imageHeight);
+	//read_image_template<int>(argv[1], &hostInputImage, &imageWidth, &imageHeight);
 
 	// Set image size information
-	int img_size = imageWidth * imageHeight * sizeof(int);
+	//int img_size = imageWidth * imageHeight * sizeof(int);
 
 	// Allocate memory for image on GPU
 	//cudaMalloc((void **)&deviceInputImage, img_size);
@@ -136,6 +137,6 @@ int main (int argc, char **argv)
 	
 	/********Encoding********/
 	
-    */
+    
 	return 0;
 }
